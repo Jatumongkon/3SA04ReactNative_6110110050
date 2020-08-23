331a14d4bfa4fd1133c6cc80848865aacdec1fd0
Jatumongkon <60840487+Jatumongkon@users.noreply.github.com>
2020-08-23 13:07:34 +0700

    first commit

--- App.js
+++ App.js
@@ -3,10 +3,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+const doIt = () =>{
+  console.log("Hello from comsole")
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text onPress={doIt}>Hello world</Text>
+       <StatusBar style="auto" /> 
     </View>
   );
 }
